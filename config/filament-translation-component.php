1bef486a0efd56181c4cce247db854a877647ad8
Siméon DAOUDA <simeondaouda@gmail.com>
2024-11-09 23:11:21 +0100

    final

---
+++ config/filament-translation-component.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+   "languages" => [
+       "en" => [
+           "label" => "English",
+           "flag" => "us"
+       ],
+       "fr" => [
+           "label" => "Français",
+           "flag" => "fr"
+       ],
+       "ar" => [
+           "label" => "Arabic",
+           "flag" => "eg"
+       ],
+       "pt_BR" => [
+           "label" => "Português (Brasil)",
+           "flag" => "br"
+       ],
+       "my" => [
+           "label" => "Burmese",
+           "flag" => "mm"
+       ],
+   ]
+];
